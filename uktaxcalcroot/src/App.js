bb9696ef3f89b4096208253df34ec22a4bbed8d2
George Whiting <gwhi94@gmail.com>
2023-10-21 20:57:35 +0100

    Structure and nav

---
+++ uktaxcalcroot/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import Navbar from './Navigation/Navbar'
+import './App.css';
+import "./index.css";
+
+function App() {
+  return (
+
+    <div>
+      <Navbar />
+    
+    </div>
+
+  );
+}
+
+export default App;
+
+
